aaa4ae27a3e1f4987808118a0c1dbcafde0ee351
Your Name <sohelkhan20939@gmail.com>
2022-07-23 18:45:21 +0530

    new change

---
+++ Arrays.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Arrays {
+    public static void main(String args[]) {
+        int marks[] = new int[6];
+        marks[0] = 12;
+        marks[1] = 15;
+        marks[2] = 33;
+        marks[3] = 155;
+        marks[4] = 126;
+        marks[5] = 120;
+
+        for (int i = 0; i < 6; i++) {
+            System.out.println(marks[i]);
+        }
+    }
+}

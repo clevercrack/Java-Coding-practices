ec3a75c38506f55f5a2ce7bea3ed364ad01ed903
Your Name <sohelkhan20939@gmail.com>
2022-07-25 17:54:43 +0530

    Concatenation of String

---
+++ StringConcate.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class StringConcate {
+    public static void main(String args[]) {
+        String firstName = "Zehra";
+        String lastName = "Naim";
+        String fullName = firstName + " " + lastName; // Concatenation of String{we use a empty
+        System.out.println("YOur full name is " + fullName);// string here for better output}
+
+    }
+}

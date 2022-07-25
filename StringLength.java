1a3d977d7a7b9353f365a90a31b4594cfa92928c
Your Name <sohelkhan20939@gmail.com>
2022-07-25 18:25:24 +0530

    Length of String

---
+++ StringLength.java
@@ -0,0 +1,8 @@
+public class StringLength {
+    public static void main(String args[]) {
+        String firstName = "Zehra";
+        String lastName = "Naim";
+        String fullName = firstName + "@@@" + lastName;
+        System.out.println(fullName.length());
+    }
+}

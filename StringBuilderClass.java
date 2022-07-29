43f64048a0b97b2d6e08d2c15994f46fa95dbdb0
Your Name <sohelkhan20939@gmail.com>
2022-07-29 21:29:32 +0530

    String Builder Class

--- StringBuilderClass.java
+++ StringBuilderClass.java
@@ -9,5 +9,17 @@ class StringBuilderClass {
         // set char at index 0
         str.setCharAt(0, 'v');
         System.out.println(str);
+
+        // insert at any index using insert() function
+        str.insert(1, 'Y');
+        System.out.println(str);
+
+        // delete at any index using delete() function
+        str.delete(1, 2);
+        System.out.println(str);
+
+        // to add in the end of any string using append() function
+        str.append(" HARRY POTTER");
+        System.out.println(str);
     }
 }
